08ffffe89766d24c042ec7b8c269bdc6399f31ab
QuentinDAMMAN <quentindamman59@gmail.com>
2021-04-08 11:10:26 +0200

    back spring boot app + maj git ignore

---
+++ Back/todolifeback/src/main/java/com/cda/todolife/TodolifebackApplication.java
@@ -0,0 +1,13 @@
+package com.cda.todolife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolifebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolifebackApplication.class, args);
+	}
+
+}
