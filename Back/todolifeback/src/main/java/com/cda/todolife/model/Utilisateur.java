952dcc91c2bb676ff7da73039ac6c08df3f9c00b
PaulSckbdt <74761872+PaulSckbdt@users.noreply.github.com>
2021-04-12 14:03:00 +0200

    ajout role dans utilisateur

--- Back/todolifeback/src/main/java/com/cda/todolife/model/Utilisateur.java
+++ Back/todolifeback/src/main/java/com/cda/todolife/model/Utilisateur.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -41,5 +42,8 @@ public class Utilisateur implements Serializable {
 	private String email;
 	private String username;
 	private String password;
+
+	@ManyToOne
+	private Role role;
 
 }
