f106da16b9b31f5f8feedcceca89be953aa0b1e0
PaulSckbdt <74761872+PaulSckbdt@users.noreply.github.com>
2021-04-12 12:11:29 +0200

    Update UtilisateurController.java

--- Back/todolifeback/src/main/java/com/cda/todolife/controller/UtilisateurController.java
+++ Back/todolifeback/src/main/java/com/cda/todolife/controller/UtilisateurController.java
@@ -1,25 +1,30 @@
 package com.cda.todolife.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.ConstraintViolationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cda.todolife.dto.UtilisateurDto;
 import com.cda.todolife.dto.UtilisateurDtoList;
 import com.cda.todolife.exception.ResourceAlreadyExist;
 import com.cda.todolife.exception.ResourceNotFoundException;
-import com.cda.todolife.exception.SerieIntrouvableException;
 import com.cda.todolife.service.IUtilisateurService;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -72,13 +77,26 @@ public class UtilisateurController {
 
 	// delete
 	@DeleteMapping("/utilisateurs/{id}")
-	public ResponseEntity<HttpStatus> delete(@PathVariable int id) throws SerieIntrouvableException {
+	public ResponseEntity<HttpStatus> delete(@PathVariable int id) throws ResourceAlreadyExist {
 		try {
 			this.utilisateurService.delete(id);
 			return new ResponseEntity<>(HttpStatus.OK);
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(ConstraintViolationException.class)
+	public Map<String, String> handleValidationExceptions(ConstraintViolationException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getConstraintViolations().forEach(constraintViolation -> {
+			String fieldName = constraintViolation.getPropertyPath().toString();
+			fieldName = fieldName.substring(fieldName.lastIndexOf('.') + 1);
+			String errorMessage = constraintViolation.getMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return errors;
 	}
 
 }
