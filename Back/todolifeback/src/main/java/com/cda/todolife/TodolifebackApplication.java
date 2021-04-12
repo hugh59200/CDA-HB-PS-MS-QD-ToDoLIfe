bd2d4f9d6e0d7ab55f8ed15a42610400585f0898
PaulSckbdt <74761872+PaulSckbdt@users.noreply.github.com>
2021-04-12 12:11:19 +0200

    mise en place swagger

--- Back/todolifeback/src/main/java/com/cda/todolife/TodolifebackApplication.java
+++ Back/todolifeback/src/main/java/com/cda/todolife/TodolifebackApplication.java
@@ -5,17 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
-@Configuration
-@EnableWebMvc
 public class TodolifebackApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
@@ -30,7 +27,8 @@ public class TodolifebackApplication extends SpringBootServletInitializer {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.cda.controller")).paths(PathSelectors.any()).build();
+				.apis(RequestHandlerSelectors.basePackage("com.cda.todolife.controller")).paths(PathSelectors.any())
+				.build();
 	}
 
 }
