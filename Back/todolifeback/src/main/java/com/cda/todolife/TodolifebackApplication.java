eb4afcd9950df170cff5b7fdcb050dcd3ab6538b
PaulSckbdt <74761872+PaulSckbdt@users.noreply.github.com>
2021-04-12 09:36:45 +0200

    mise en place swaggerui

--- Back/todolifeback/src/main/java/com/cda/todolife/TodolifebackApplication.java
+++ Back/todolifeback/src/main/java/com/cda/todolife/TodolifebackApplication.java
@@ -3,28 +3,34 @@ package com.cda.todolife;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
-public class TodolifebackApplication {
+@Configuration
+@EnableWebMvc
+public class TodolifebackApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolifebackApplication.class, args);
 	}
-	
-	
+
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
-	
-//	@Bean
-//	public Docket api() {
-//	     return new Docket(DocumentationType.SWAGGER_2)
-//	         .select()
-//	         .apis(RequestHandlerSelectors.basePackage("com.cda.todolife.controller"))
-//	         .paths(PathSelectors.any())
-//	         .build();
-//	 }
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.cda.controller")).paths(PathSelectors.any()).build();
+	}
 
 }
