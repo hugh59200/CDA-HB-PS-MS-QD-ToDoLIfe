5659fce05d74b16afd0401d6eb9c82efcd9518d3
hugo BOGRAND <h.bogrand@gmail.com>
2021-05-02 15:41:18 +0200

    update AjourJour

---
+++ Front/todolifefront/src/components/journal/fonctions/DetailJour.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { bouttonRevenir } from './Bouttons';
+import { mood } from './Mood';
+
+function DetailJour(jourData, setshowList, setshowJourDetail) {
+	return (
+		<>
+			<div className="jourdetails">
+				<div className="enteteJour">
+					<div className="jourData">{mood(jourData.humeur)}</div>
+					<div className="titreJour">{jourData.titre}</div>
+				</div>
+				<div className="textJour">
+					<p className="jourDataTexte">{jourData.texte}</p>
+				</div>
+			</div>
+			{bouttonRevenir(setshowList, setshowJourDetail)}
+		</>
+	);
+}
+export default DetailJour;
