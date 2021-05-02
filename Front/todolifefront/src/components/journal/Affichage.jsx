8fcf623b3fc9e26f1759a77043c745935b37c520
hugo BOGRAND <h.bogrand@gmail.com>
2021-05-02 23:29:55 +0200

    Revert "update journal -> component partie 8"
    This reverts commit 8fca001cabd4d39de57d9e145388ca835f82c2e7.

--- Front/todolifefront/src/components/journal/Affichage.jsx
+++ Front/todolifefront/src/components/journal/Affichage.jsx
@@ -5,31 +5,27 @@ import { AjouterJour } from './fonctions/affichages/AjouterJour';
 import { DetailJour } from './fonctions/affichages/DetailJour';
 
 export function Affichage(props) {
-	const [jourData, setjourData] = useState('');
 	const [ajoutJour, setajoutJour] = useState(false);
 	const [showJourDetail, setshowJourDetail] = useState(false);
-
 	if (props.showList)
 		return (
 			<AffichageListe
 				loading={props.loading}
 				data={props.data}
-				setjourData={setjourData}
+				setjourData={props.setjourData}
 				setshowList={props.setshowList}
 				setshowJourDetail={setshowJourDetail}
 				setajoutJour={setajoutJour}
 			/>
 		);
-
 	if (showJourDetail)
 		return (
 			<DetailJour
-				jourData={jourData}
+				jourData={props.jourData}
 				setshowList={props.setshowList}
 				setshowJourDetail={setshowJourDetail}
 			/>
 		);
-
 	if (ajoutJour)
 		return (
 			<AjouterJour
--- Front/todolifefront/src/components/journal/MonJournal.jsx
+++ Front/todolifefront/src/components/journal/MonJournal.jsx
@@ -6,19 +6,31 @@ import { API_JOURNAL_BY_USERID } from '../../constant/API_BACK';
 import { Affichage } from './fonctions/affichages/Affichage';
 import { Selects } from './fonctions/selects/SelectDate';
 
+// import { AffichageListe } from './fonctions/listeJour/AffichageListe';
+// import { AjouterJour } from './fonctions/affichages/AjouterJour';
+// import DetailJour from './fonctions/affichages/DetailJour';
+
+
+
 const MonJournal = () => {
+	const [mois, setmois] = useState(new Date().getMonth());
+	const [annee, setannee] = useState(new Date().getFullYear());
 	const [loading, setLoading] = useState(true);
 	const [showList, setshowList] = useState(true);
+	const [jourData, setjourData] = useState('');
 	const [data, setData] = useState([]);
+	// const [ajoutJour, setajoutJour] = useState(false);
+	// const [showJourDetail, setshowJourDetail] = useState(false);
 
 	return (
 		<>
 			<h2 className="titreJournal">Mon journal</h2>
 			<div className="monJournal">
 				<Selects
+					FetchUrl={FetchUrl}
+					annee={annee}
 					setshowList={setshowList}
-					setLoading={setLoading}
-					setData={setData}
+					mois={mois}
 				/>
 				<Affichage
 					showList={showList}
