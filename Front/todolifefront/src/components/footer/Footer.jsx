baf41230b3dbde07e27de183039e0ac2bca1d030
hugo BOGRAND <h.bogrand@gmail.com>
2021-05-02 15:54:58 +0200

    bugFix footer (p -> div)

--- Front/todolifefront/src/components/footer/Footer.jsx
+++ Front/todolifefront/src/components/footer/Footer.jsx
@@ -1,12 +1,11 @@
-
 import React from "react";
 
 const Footer = () => (
   <div className="footer text-center text-white">
-    <p>
+    <div>
     <h4>© Copyright ToDoLife</h4>
     <h6>Made by Hugo BOGRAND, Matéo SABA, Paul SACKEBANDT, Quentin DAMMAN</h6>
-    </p>
+    </div>
   </div>
 );
 
