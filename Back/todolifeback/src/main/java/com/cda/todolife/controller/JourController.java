ad7ce4a748df17e64ed769464a122417aea6565b
hugo BOGRAND <h.bogrand@gmail.com>
2021-05-03 16:22:33 +0200

    update ajoutJour ( envoi donnée et response ok)

--- Back/todolifeback/src/main/java/com/cda/todolife/controller/JourController.java
+++ Back/todolifeback/src/main/java/com/cda/todolife/controller/JourController.java
@@ -24,10 +24,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cda.todolife.dto.JourDto;
-import com.cda.todolife.dto.JournalDto;
 import com.cda.todolife.exception.JourExistantException;
 import com.cda.todolife.exception.JourIntrouvableException;
 import com.cda.todolife.exception.JournalIntrouvableException;
+import com.cda.todolife.exception.ResourceNotFoundException;
 import com.cda.todolife.service.IJourService;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -51,31 +51,6 @@ public class JourController {
 		this.jourService.add(jour);
 		return ResponseEntity.ok(jour);
 	}
-//	// create
-//	@PostMapping("/jour")
-//	public ResponseEntity<JourDto> create(@RequestParam(value = "titre") String titre,
-//			@RequestParam(value = "humeur") int humeur, @RequestParam(value = "texte") String texte,
-//			@RequestParam(value = "id") int id) throws JourExistantException, JournalIntrouvableException {
-//		
-//		JourDto jour = new JourDto();
-//		SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd");
-//		Date now = new Date();
-//		jour.setDateJour(sdfDate.format(now));
-//		jour.setTitre(titre);
-//		jour.setHumeur(humeur);
-//		jour.setTexte(texte);
-//		
-//		JournalDto journal = this.journalService.findById(id);
-//		List<JourDto> list = journal.getJours();
-//		list.add(jour);
-//		journal.setJours(list);
-//		System.out.println(titre);
-//		System.out.println(humeur);
-//		System.out.println(id);
-//		
-//		this.jourService.add(jour);
-//		return ResponseEntity.ok(jour);
-//	}
 
 	// details by Id
 	@GetMapping("/jour/{id}")
