7bedb30fdfa0a03fbfbf53c46099afcfa6a25335
hugo BOGRAND <h.bogrand@gmail.com>
2021-05-14 11:18:58 +0200

    mep modificationJour partie 5.2

--- Front/todolifefront/src/components/journal/fonctions/bouttons/BouttonModifier.jsx
+++ Front/todolifefront/src/components/journal/fonctions/bouttons/BouttonModifier.jsx
@@ -1,41 +1,41 @@
 export function BouttonModifier(props) {
-	const dateJour = '';
-	const titre = props.titreUpdateValue;
-	const humeur = props.moodUpdateValue;
-	const texte = props.resumeUpdateValue;
-	
-	const jour = {
-		dateJour,
-		titre,
-		humeur,
-		texte,
-	};
-	
 	return (
 		<button
 			className="btn-form"
 			onClick={() => {
-				handleSubmit(props, jour);
+				handleSubmit(props);
 			}}
 		>
 			modifier
 		</button>
 	);
+}
 
-	function handleSubmit(props, jour) {
-		console.log(props.settitreUpdateValue.length);
-		if (props.settitreUpdateValue.length > 1) {
-			console.log("ok");
-			// props.setshowList(true);
-			// props.setajoutJour(false);
-			// props.setupdateJour(false);
-			updateJour(jour);
-		} else {
-			props.setavertissement("n'oubliez pas le titre")
-		}
+function handleSubmit(props) {
+	if (props.titreUpdateValue.length !== 0) {
+		updateJour(props);
+	} else {
+		props.setavertissement("n'oubliez pas le titre");
 	}
 }
 
-function updateJour(jour) {
-	console.log(jour);
+function updateJour(props) {
+	// props.setshowList(true);
+	// props.setajoutJour(false);
+	// props.setupdateJour(false);
+	// const dateJour = '';
+
+	const titre = props.titreUpdateValue;
+	// const humeur = props.moodUpdateValue;
+	const texte = props.resumeUpdateValue;
+
+	const jour = {
+		// dateJour,
+		titre,
+		// humeur,
+		texte,
+	};
+
+	console.log(jour.titre);
+	console.log(jour.texte);
 }
