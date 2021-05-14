85b25adb7d9aaf482b835e5d5d9fd4134f620481
hugo BOGRAND <h.bogrand@gmail.com>
2021-05-14 10:07:46 +0200

    mep modificationJour partie 5

--- Front/todolifefront/src/components/journal/fonctions/affichages/detailsJour/DetailJour.jsx
+++ Front/todolifefront/src/components/journal/fonctions/affichages/detailsJour/DetailJour.jsx
@@ -11,101 +11,93 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Mood } from '../../autres/Mood';
 
 export function DetailJour(props) {
-	const [titreUpdate, settitreUpdate] = useState(false);
-	const [titreUpdateValue, settitreUpdateValue] = useState(
-		props.jourData.titre,
-	);
-	const [moodUpdateValue, setmoodUpdateValue] = useState();
-	const [resumeUpdate, setresumeUpdate] = useState(false);
-	const [resumeUpdateValue, setresumeUpdateValue] = useState();
+	// const [titreUpdate, settitreUpdate] = useState(false);
+	// const [titreUpdateValue, settitreUpdateValue] = useState(
+	// 	props.jourData.titre,
+	// );
+	// const [moodUpdateValue, setmoodUpdateValue] = useState();
+	// const [resumeUpdate, setresumeUpdate] = useState(false);
+	// const [resumeUpdateValue, setresumeUpdateValue] = useState();
 
-	return (
-		<div className="jourdetails">
-			<div className="enteteJour">
-				<div className="jourData">
-					{props.updateJour && (
+	if (!props.updateJour) {
+		return (
+			<div className="jourdetails">
+				<div className="enteteJour">
+					<Mood moodLevel={props.jourData.humeur} className="jourData" />
+					<div className="titreJour">
+						<p>{props.jourData.titre}</p>
+					</div>
+				</div>
+				<div className="textJour">
+					<p className="jourDataTexte">{props.jourData.texte}</p>
+				</div>
+				<div className="deuxBoutons">
+					<BouttonRevenir
+						setshowList={props.setshowList}
+						setshowJourDetail={props.setshowJourDetail}
+						setajoutJour={props.setajoutJour}
+						setupdateJour={props.setupdateJour}
+					/>
+				</div>
+			</div>
+		);
+	} else if (props.updateJour) {
+		return (
+			<div className="jourdetails">
+				<div className="enteteJour">
+					<div className="jourData">
 						<FontAwesomeIcon
 							icon={faArrowAltCircleLeft}
 							className="fleche"
 							onClick={() => {}}
 						/>
-					)}
-					<Mood moodLevel={props.jourData.humeur} />
-					{props.updateJour && (
+						<Mood moodLevel={props.jourData.humeur} />
 						<FontAwesomeIcon
 							icon={faArrowAltCircleRight}
 							className="fleche"
 							onClick={() => {}}
 						/>
-					)}
-				</div>
-				<div className="titreJour">
-					{props.updateJour && !titreUpdate && (
-						<>
-							<p>{titreUpdateValue}</p>
-							<FontAwesomeIcon
-								icon={faPen}
-								className="updateTitre"
-								onClick={() => {
-									settitreUpdate(true);
-								}}
-							/>
-						</>
-					)}
-					{props.updateJour && titreUpdate && (
+					</div>
+					<div className="titreJour">
 						<input
 							type="text"
 							className="inputTitreUpdate"
 							id={props.id}
-							placeholder="nouveau titre"
+							placeholder={props.jourData.titre}
 							placeholderTextColor="red"
-							onChange={e => settitreUpdateValue(e.target.value)}
+							// onChange={e => settitreUpdateValue(e.target.value)}
 						></input>
-					)}
+					</div>
 				</div>
-			</div>
-			<div className="textJour">
-				{props.updateJour && !resumeUpdate && (
-					<p
-						className="jourDataTexte"
-						onClick={() => {
-							setresumeUpdate(true);
-						}}
-					>
-						{props.jourData.texte}
-					</p>
-				)}
-				{props.updateJour && resumeUpdate && (
+				<div className="textJour">
 					<div className="choixResume">
 						<textarea
 							name=""
 							cols="30"
 							rows="10"
-							placeholder="Alors cette journée ?"
-							onChange={e => props.setresumeUpdateValue(e.target.value)}
+							placeholder={props.jourData.texte}
+							// onChange={e => props.setresumeUpdateValue(e.target.value)}
 						></textarea>
 					</div>
-				)}
-			</div>
-			<div className="deuxBoutons">
-				<BouttonRevenir
-					setshowList={props.setshowList}
-					setshowJourDetail={props.setshowJourDetail}
-					setajoutJour={props.setajoutJour}
-					setupdateJour={props.setupdateJour}
-				/>
-				{props.updateJour && (
+				</div>
+				<div className="deuxBoutons">
+					<BouttonRevenir
+						setshowList={props.setshowList}
+						setshowJourDetail={props.setshowJourDetail}
+						setajoutJour={props.setajoutJour}
+						setupdateJour={props.setupdateJour}
+					/>
 					<BouttonModifier
-						titreUpdateValue={titreUpdateValue}
-						moodUpdateValue={moodUpdateValue}
-						resumeUpdateValue={resumeUpdateValue}
+						// titreUpdateValue={titreUpdateValue}
+						// moodUpdateValue={moodUpdateValue}
+						// resumeUpdateValue={resumeUpdateValue}
 						// setupdateJour={props.setupdateJour}
 						// setshowList={props.setshowList}
 						// setshowJourDetail={props.setshowJourDetail}
 						// setajoutJour={props.setajoutJour}
 					/>
-				)}
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
