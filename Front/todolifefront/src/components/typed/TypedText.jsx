aa5debc2bafa0c21173fc6b4217e8ee412da976e
MateoSaba <mtosaba@gmail.com>
2021-05-17 16:30:25 +0200

    modif charte graphique + ajout du nouveau logo

--- Front/todolifefront/src/components/typed/TypedText.jsx
+++ Front/todolifefront/src/components/typed/TypedText.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Typed from "react-typed";
+import '../../views/home/Home.css'
 class TypedText extends Component {
   render() {
     if (localStorage.getItem("username") == null) {
       return (
         /* soit typed soit text me one sans typed, a voir ensemble */
-        <Typed
+        <Typed className="fonthome"
           strings={["Bienvenue  <br/>  sur  ToDoLife."]}
           typeSpeed={40}
           backSpeed={80}
@@ -16,7 +17,7 @@ class TypedText extends Component {
     } else {
       return (
         /* soit typed soit text me one sans typed, a voir ensemble */
-        <Typed
+        <Typed className="fonthome"
           strings={[
             "Bienvenue " +
               localStorage.getItem("username") +
