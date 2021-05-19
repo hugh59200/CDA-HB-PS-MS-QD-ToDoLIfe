46ebca5b5f4b8668a86677e24dcb23d0df4b78c0
QuentinDAMMAN <quentindamman59@gmail.com>
2021-05-19 19:35:51 +0200

    création des controlleurs (aucune méthodes)

---
+++ Back/todolifeback/src/main/java/com/cda/todolife/controller/StatistiquesController.java
@@ -0,0 +1,12 @@
+package com.cda.todolife.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/api")
+public class StatistiquesController {
+
+}
