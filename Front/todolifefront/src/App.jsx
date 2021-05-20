e4947a9ef3e0bd56a3fc8649b24f8a697fe47092
hugo BOGRAND <h.bogrand@gmail.com>
2021-05-20 15:04:58 +0200

    bugfix background image

--- Front/todolifefront/src/App.jsx
+++ Front/todolifefront/src/App.jsx
@@ -1,16 +1,13 @@
-import { BrowserRouter as Router} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import "react-toastify/dist/ReactToastify.css"
-
 import '../src/assets/css/footer/footer.css'
 
-
-import Routes from './routes/Routes';
-import NavBar from './components/navBar/NavBar'
 import Footer from './components/footer/Footer';
-
+import NavBar from './components/navBar/NavBar'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes/Routes';
 
 function App() {
   return (
