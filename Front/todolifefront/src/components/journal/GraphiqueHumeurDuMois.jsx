c96a7eaecf6f009e790d7ccc9e16825e91a81a92
hugo BOGRAND <h.bogrand@gmail.com>
2021-05-21 11:05:38 +0200

    mep tableau de bord journal partie 4

--- Front/todolifefront/src/components/journal/GraphiqueHumeurDuMois.jsx
+++ Front/todolifefront/src/components/journal/GraphiqueHumeurDuMois.jsx
@@ -1,6 +1,8 @@
 import { Line } from 'react-chartjs-2';
 import React from 'react';
 
+// import axios from 'axios';
+
 const state = {
 	labels: ['January', 'February', 'March', 'April', 'May'],
 	datasets: [
@@ -16,31 +18,82 @@ const state = {
 	],
 };
 
-function handleClick(nbJours) {
-	
-	// history.push(lien);
-}
-
 export const GraphiqueHumeurDuMois = () => {
+	
+		// const [chartData, setChartData] = useState({});
+		// const moodEchelle = [1, 2, 3, 4];
+		// const [duration, setduration] = useState();
+		// const [mood, setmood] = useState([]);
+		// const [dateJour, setdateJour] = useState([]);
+		
+		// const chart = () => {
+		// 	let mood = [];
+		// 	let empAge = [];
+		// 	axios
+		// 		.get('http://dummy.restapiexample.com/api/v1/employees')
+		// 		.then(res => {
+		// 			console.log(res);
+		// 			for (const dataObj of res.data.data) {
+		// 				mood.push(parseInt(dataObj.mood));
+		// 				dateJour.push(parseInt(dataObj.dateJour));
+		// 			}
+		// 			setChartData({
+		// 				labels: empAge,
+		// 				datasets: [
+		// 					{
+		// 						label: 'level of thiccness',
+		// 						data: dateJour,
+		// 						backgroundColor: ['rgba(75, 192, 192, 0.6)'],
+		// 						borderWidth: 4,
+		// 					},
+		// 				],
+		// 			});
+		// 		})
+		// 		.catch(err => {
+		// 			console.log(err);
+		// 		});
+		// 	// console.log(empSal, empAge);
+		// };
+		
 	return (
 		<div className="statistique">
 			<div className="titre">Statistiques</div>
 			<div className="choixEchelle">
 				<div>
-					<div className="echelleGraph" onClick={() => {
-            handleClick(7);
-          }}>ces 7 derniers jours</div>
-					<div className="echelleGraph" onClick={() => {
-            handleClick(30);
-          }}>ce mois-ci</div>
+					<div
+						className="echelleGraph"
+						onClick={() => {
+							// setduration(7);
+						}}
+					>
+						ces 7 derniers jours
+					</div>
+					<div
+						className="echelleGraph"
+						onClick={() => {
+							// setduration(30);
+						}}
+					>
+						ce mois-ci
+					</div>
 				</div>
 				<div>
-					<div className="echelleGraph" onClick={() => {
-            handleClick(90);
-          }}>ces 3 derniers mois</div>
-					<div className="echelleGraph" onClick={() => {
-            handleClick(365);
-          }}>cette année</div>
+					<div
+						className="echelleGraph"
+						onClick={() => {
+							// setduration(90);
+						}}
+					>
+						ces 3 derniers mois
+					</div>
+					<div
+						className="echelleGraph"
+						onClick={() => {
+							// setduration(365);
+						}}
+					>
+						cette année
+					</div>
 				</div>
 			</div>
 			<div className="graphique">
@@ -55,7 +108,7 @@ export const GraphiqueHumeurDuMois = () => {
 						legend: {
 							display: true,
 							position: 'right',
-							color:"white"
+							color: 'white',
 						},
 					}}
 				/>
