2d434e57f5689a052dc1126951aa42f132efbff4
PaulSckbdt <74761872+PaulSckbdt@users.noreply.github.com>
2021-05-25 10:38:55 +0200

    model routine

---
+++ Back/todolifeback/src/main/java/com/cda/todolife/model/routine/Soir.java
@@ -0,0 +1,32 @@
+package com.cda.todolife.model.routine;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.cda.todolife.model.Utilisateur;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Soir {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idElemSoir;
+	private int heure;
+	private String texte;
+
+	@ManyToOne
+	@JoinColumn(name = "id_utilisateur", nullable = false)
+	private Utilisateur utilisateur;
+}
